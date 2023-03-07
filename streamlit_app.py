a5b30134147c02e037fe62bf02a2715655ae23af
cheny0501 <112567460+cheny0501@users.noreply.github.com>
2023-03-06 21:53:25 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,99 +5,49 @@ import streamlit as st
 # Data pre-processing
 
 @st.cache_data
-def load_data():
-    df1 = pd.read_csv("https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-counties-2021.csv")
+def load_data1():
+    # read data
+    df_case = pd.read_csv("https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-counties-2021.csv")
     df_vac = pd.read_csv("https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/vaccinations/us_state_vaccinations.csv")
     
     # start from 2021-01-12
-    df1 = df1[df1['date'] > '2021-01-11']
+    # since the record for df_vac began from 2021-01-12
+    df_case = df_case[df_case['date'] > '2021-01-11']
     # groupby cases and deaths for same date
-    df1 = df1.groupby(['date','state']).agg({'cases': 'sum', 'deaths': 'sum'})
+    df_case = df_case.groupby(['date','state']).agg({'cases': 'sum', 'deaths': 'sum'})
+    # subset only useful columns
     df_vac = df_vac[[ "date", "location","total_vaccinations", "total_vaccinations_per_hundred" ]]
+    # rename to be consistent for merging
     df_vac = df_vac.rename(columns={"location": "state"})
-    df_new = df1.merge(df_vac, on=['date', 'state'], how='left')
-    df_new.dropna(inplace=True)
-    df["date"] = pd.to_datetime(df["date"]).astype(int) / 10**9 
-    return df
+    # merge the two datasets
+    df1 = df_case.merge(df_vac, on=['date', 'state'], how='left')
+    # drop all missing values
+    df1.dropna(inplace=True)
+    # change date type
+    df1["date"] = pd.to_datetime(df["date"]).astype(int) / 10**9 
+    # Group the data by state and date
+    grouped = df1.groupby(['state', 'date'])
+    # Calculate the mortality rate for each state and date
+    mortality_rates = (grouped['deaths'].sum() / grouped['cases'].sum()) * 100
+    # Add the mortality rates as a new column to the original DataFrame
+    df1['case_fatality_rate'] = df1.set_index(['state', 'date']).index.map(mortality_rates.get)
+    df_wide = df1
+    
+    return df_wide
 
-df = load_data()
+df_wide = load_data1()
 
-### Title ###
+@st.cache_data
+def load_data2():
+# change df to long
+    df_long = df1.melt(id_vars=['date', 'state'], value_vars=['cases', 'deaths', 'total_vaccinations', 'total_vaccinations_per_hundred', 'mortality'], var_name='selection')
+    
+    return df_long
 
-st.write("## Geographical Distribution")
-st.write("### Group: Viz or DY")
+df_long = load_data2()    
 
-### Task 1 ###
+def load_data3():
+  task1_df = df_long[df_long['selection'].isin(['cases', 'deaths'])]
+  return task1_df
 
-st.write("### What’s the trend of COVID-19 cases and deaths over time in the US?")
-
-# replace with st.slider
-date = st.slider("date", int(df["date"].min()), int(df["date"].max()), int(df["date"].min()))
-subset = df[df["date"] == date]
-### P2.1 ###
-
-
-### P2.2 ###
-# replace with st.radio
-sex = st.radio("Sex", ("M", "F"))
-subset = subset[subset["Sex"] == sex]
-### P2.2 ###
-
-
-### P2.3 ###
-# replace with st.multiselect
-# (hint: can use current hard-coded values below as as `default` for selector)
-default = [
-    "Austria",
-    "Germany",
-    "Iceland",
-    "Spain",
-    "Sweden",
-    "Thailand",
-    "Turkey",
-]
-countries = st.multiselect('Countries', subset["Country"].unique(), default)
-subset = subset[subset["Country"].isin(countries)]
-### P2.3 ###
-
-
-### P2.4 ###
-# replace with st.selectbox
-cancer_options = df["Cancer"].unique()
-cancer = st.selectbox("Cancers", cancer_options)
-subset = subset[subset["Cancer"] == cancer]
-### P2.4 ###
-
-
-### P2.5 ###
-ages = [
-    "Age <5",
-    "Age 5-14",
-    "Age 15-24",
-    "Age 25-34",
-    "Age 35-44",
-    "Age 45-54",
-    "Age 55-64",
-    "Age >64",
-]
-
-chart = alt.Chart(subset).mark_rect().encode(
-    x=alt.X("Age", sort=ages),
-    y=alt.Y("Country"),
-    color=alt.Color("Rate", scale=alt.Scale(type='log',domain=(0.01,1000),clamp=True), legend=alt.Legend(title="Mortality rate per 100k")),
-    tooltip=["Rate"],
-).properties(
-    title=f"{cancer} mortality rates for {'males' if sex == 'M' else 'females'} in {year}",
-)
-
-### P2.5 ###
-
-st.altair_chart(chart, use_container_width=True)
-
-countries_in_subset = subset["Country"].unique()
-if len(countries_in_subset) != len(countries):
-    if len(countries_in_subset) == 0:
-        st.write("No data avaiable for given subset.")
-    else:
-        missing = set(countries) - set(countries_in_subset)
-        st.write("No data available for " + ", ".join(missing) + ".")
+task1_df = load_data3()    
