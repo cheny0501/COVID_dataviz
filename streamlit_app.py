af4e71b37395b735c86d73a3a5db425cd0b04a5e
Tony Ding <78240133+Tony-Xiayi-Ding@users.noreply.github.com>
2023-03-07 23:31:52 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -87,7 +87,9 @@ st.write("#### Tony Ding, Chen Yang")
 #### Task1 ###
 st.write("#### Task1: What’s the trend of COVID-19 cases and deaths over time in the US?")
 # Create the chart
-chart = alt.Chart(task1_df).mark_area().encode(
+chart = alt.Chart(task1_df).mark_area(color = 'green',
+                           opacity = 0.5,
+                           line = {'color':'darkgreen'}).encode(
     x='date:T',
     y="value:Q",
     color="selection:N",
